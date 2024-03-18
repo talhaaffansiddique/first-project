16296e04f70966269ce3a65eb8ea00caf51efad0
Talha Affan Siddique <talhaaffansiddique@gmail..com>
2024-03-18 15:01:11 +0500

    34. Proper List

--- src/index.js
+++ src/index.js
@@ -4,40 +4,26 @@ import ReactDOM from "react-dom";
 //CSS
 import "./index.css";
 
-const firstBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
-  title: "title will be discovered soon",
-  author: "Talha ",
-};
+const books = [
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+    title: "title will be discovered soon",
+    author: "Talha ",
+  },
 
-const secondBook = {
-  img: "https://m.media-amazon.com/images/I/914661eRCnL._AC_UY218_.jpg",
-  title: "The Family Secret",
-  author: "Kiersten Modglin ",
-};
+  {
+    img: "https://m.media-amazon.com/images/I/914661eRCnL._AC_UY218_.jpg",
+    title: "The Family Secret",
+    author: "Kiersten Modglin ",
+  },
+];
+const names = ["john", "peter", "susan"];
+const newName = names.map((name) => {
+  return <h1>{name}</h1>;
+});
 
 function BookList() {
-  return (
-    <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias
-          hic nulla fugit quibusdam! Odio inventore molestias, vel iste tenetur
-          repudiandae quasi blanditiis ducimus totam nobis ipsa voluptate illum,
-          quis quos?
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-    </section>
-  );
+  return <section className="booklist">{newName}</section>;
 }
 
 const Book = (props) => {
