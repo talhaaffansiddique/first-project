3c4a0960a44d31ba0ee7f86550e88fb90894af24
Talha Affan Siddique <talhaaffansiddique@gmail..com>
2024-03-19 10:53:06 +0500

    37. Event Basics

--- src/index.js
+++ src/index.js
@@ -27,18 +27,41 @@ const newName = names.map((name) => {
 function BookList() {
   return (
     <section className="booklist">
+      <EventExamples />
       {books.map((book) => {
-        const { img, title, author, id } = book;
-
-        return <Book img={img} title={title} aurthor={author} key={id} />;
+        return <Book {...book} key={book.id} />;
       })}
     </section>
   );
 }
 
+const EventExamples = () => {
+  const handleFormInput = () => {
+    console.log("handle form input");
+  };
+  const handleButtonClick = () => {
+    alert("handle button click");
+  };
+
+  return (
+    <section>
+      <form>
+        <h2>Typical Form</h2>
+        <input
+          type="text"
+          name="example"
+          onChange={handleFormInput}
+          style={{ margin: "1rem" }}
+        />
+      </form>
+      <button onClick={handleButtonClick}>click me </button>
+    </section>
+  );
+};
+
 const Book = (props) => {
   const { img, title, author } = props;
-  console.log(props);
+  //console.log(props);
   return (
     <article className="book">
       <img src={img} alt="" />
